921de452188410f2a8e0be20e14c4028f632746f
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-06-14 23:53:28 +0300

    почти рабочий вариант

---
+++ eWalle/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  eWalle
+//
+//  Created by Kirill Drozdov on 14.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  @IBAction func signInButtonPressed(_ sender: UIButton) {
+    print("signInButtonPressed")
+  }
+
+  @IBAction func createButtonPressed(_ sender: UIButton) {
+    print("createButtonPressed")
+
+  }
+}
